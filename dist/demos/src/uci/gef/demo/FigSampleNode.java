f1feb93622ceed5e00a81ae0d08077ee00924fb5
jdevons <jdevons@brunov.info>
2000-07-04 03:51:45 +0000

    Implemented get- and set- PrivateData functions

--- dist/demos/src/uci/gef/demo/FigSampleNode.java
+++ dist/demos/src/uci/gef/demo/FigSampleNode.java
@@ -1,3 +1,5 @@
+package uci.gef.demo;
+
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -21,8 +23,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-package uci.gef.demo;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -34,36 +34,51 @@ public class FigSampleNode extends FigNode {
   FigText obj7;
 
   public FigSampleNode() {
-    super();
-    obj1 = new FigRect(-25, -25, 50, 50, Color.black, Color.white);
-    obj2 = new FigCircle(-20, -20, 40, 40, Color.red, null);
-    obj3 = new FigCircle( -5, -30, 10, 10, Color.black, Color.blue);
-    obj4 = new FigCircle( -5,  20, 10, 10, Color.black, Color.blue);
-    obj5 = new FigRect(-30,  -5, 10, 10, Color.black, Color.green);
-    obj6 = new FigRect( 20,  -5, 10, 10, Color.black, Color.green);
-    obj7 = new FigText( -10,  -10, 20, 20);
-    obj7.setLineWidth(0);
-    obj7.setJustification(FigText.JUSTIFY_CENTER);
+	super();
+	obj1 = new FigRect(-25, -25, 50, 50, Color.black, Color.white);
+	obj2 = new FigCircle(-20, -20, 40, 40, Color.red, null);
+	obj3 = new FigCircle( -5, -30, 10, 10, Color.black, Color.blue);
+	obj4 = new FigCircle( -5,  20, 10, 10, Color.black, Color.blue);
+	obj5 = new FigRect(-30,  -5, 10, 10, Color.black, Color.green);
+	obj6 = new FigRect( 20,  -5, 10, 10, Color.black, Color.green);
+	obj7 = new FigText( -10,  -10, 20, 20);
+	obj7.setLineWidth(0);
+	obj7.setJustification(FigText.JUSTIFY_CENTER);
 
-    addFig(obj1);
-    addFig(obj2);
-    addFig(obj3);
-    addFig(obj4);
-    addFig(obj5);
-    addFig(obj6);
-    addFig(obj7);
+	addFig(obj1);
+	addFig(obj2);
+	addFig(obj3);
+	addFig(obj4);
+	addFig(obj5);
+	addFig(obj6);
+	addFig(obj7);
 
-  }
-
+  }    
+public String getPrivateData() {
+	return "text=\"" + obj7.getText() + "\"";
+}
   public void setOwner(Object own) {
-    super.setOwner(own);
-    if (!(own instanceof SampleNode)) return;
-    SampleNode node = (SampleNode) own;
-    obj7.setText(""+ node.getNumber());
-    bindPort(node.north, obj3);
-    bindPort(node.south, obj4);
-    bindPort(node.east, obj5);
-    bindPort(node.west, obj6);
-  }
-  
+	super.setOwner(own);
+	if (!(own instanceof SampleNode)) return;
+	SampleNode node = (SampleNode) own;
+	obj7.setText(""+ node.getNumber());
+	bindPort(node.north, obj3);
+	bindPort(node.south, obj4);
+	bindPort(node.east, obj5);
+	bindPort(node.west, obj6);
+  }  
+public void setPrivateData(String data) {
+	StringTokenizer tokenizer = new StringTokenizer(data,"=\"' ");
+	
+	while (tokenizer.hasMoreTokens()) {
+		String tok = tokenizer.nextToken();
+		if (tok.equals("text")) {
+			String s = tokenizer.nextToken();
+			obj7.setText( s );
+		}
+		else {
+			/* Unknown value */
+		}
+	}
+}
 } /* end class FigSampleNode */
