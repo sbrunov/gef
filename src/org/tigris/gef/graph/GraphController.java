2bf64712d4a2d52e9ae39da49dc50b038b2b750e
Thorsten Sturm <1sturm>
2000-10-16 11:42:45 +0000

    new interface GraphController

---
+++ src/org/tigris/gef/graph/GraphController.java
@@ -0,0 +1,26 @@
+
+// File: GraphController.java
+// Interfaces: GraphController
+// Original Author: thorsten Oct 2000
+// $Id$
+
+package org.tigris.gef.graph;
+
+/**
+ * This interface is the basis for each class that
+ * handles the control of pairs of data and representational objects.
+ */
+public interface GraphController extends java.io.Serializable {
+	/** 
+	 * Add a new presentation to the list of known presentations.
+	 * Each presentation consists of a data object and an object
+	 * for its graphical representation.
+	 */
+	public void addPresentation(Object representation, Object data);
+
+	/** 
+	 * Remove a presentation from the list of known presentations.
+	 */
+	public void removePresentation(Object element);
+
+} /* end interface GraphController */
