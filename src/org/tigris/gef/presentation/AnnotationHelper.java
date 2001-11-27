e17555ed28964d2329da76e6535187ba239d0dc8
jvv <jvv@brunov.info>
2001-11-27 12:43:07 +0000

    Debugged AnnotationHelper, plus: can handle FigEdgePolys

--- src/org/tigris/gef/presentation/AnnotationHelper.java
+++ src/org/tigris/gef/presentation/AnnotationHelper.java
@@ -1,88 +1,166 @@
 package org.tigris.gef.presentation;
 
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.Hashtable;
+//import java.awt.event.*;
+//import javax.swing.*;
+//import java.util.Hashtable;
 import java.awt.Point;
+import java.util.Vector;
 
+/** Contains utility methods for solving std geometry problems.
+ *
+ * The original class had several bugs and/or performance problems that were fixed.
+ * The new AnnotationHelper can handle FigPolys, ie, lines with bend points.
+ *
+ * Improvement can be made by improving FigPoly's method for getting all points.
+ *
+ * @author unknown, jesco.von.voss@gentleware.de
+ * @version 2.0
+ */
 public class AnnotationHelper{
-
-        private static AnnotationHelper theInstance = null;
-
-	private float ratio;
-	private int offset;
-	private float dd;
-	private Point a = new Point();
-	private Point normpoint = new Point();
-
-
-        private AnnotationHelper(){
+    
+    private static AnnotationHelper theInstance = null;
+    
+    
+    private AnnotationHelper(){
+    }
+    
+    public static AnnotationHelper instance(){
+        if (theInstance==null) theInstance = new AnnotationHelper();
+        return theInstance;
+    }
+    
+    /** Unmodified method. */
+    public static final int getNormOffset(Point p0, Point p1, Point p2){
+        // p0: annotation's position
+        // p1: edge's starting point
+        // p2: edge's ending point
+        float dd = (float)( ( (p2.x-p1.x)*(p0.y-p1.y) - (p0.x-p1.x)*(p2.y-p1.y) ) /
+        Math.sqrt( (p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y) )  );
+        int offset = -1 * (int) Math.round(dd);
+        //System.out.println("distance: " + d);
+        return offset;
+    }
+    
+    /** Calculates the point (as relative length) where the perpendicular hits the line.
+     *
+     * It's final so that no one can damage the implementation (and for speed).
+     *
+     * @from The point that sits in space.
+     * @begin The start point of the edge.
+     * @end The end point of the edge.
+     * @return A double: if 0, then the point 'from' sits on the perpendicular
+     *  through the point 'begin', if 1, it is on the perpendicular through
+     *  'end'. You figure out the rest.
+     */
+    public static final float getRatio(Point from, Point begin, Point end) {
+        int directionX = (end.x-begin.x);
+        int directionY = (end.y-begin.y);
+        // length of the segment, squared
+        int lengthSqr = directionX*directionX+directionY*directionY;
+        
+        // begin + diffToStart*direction is the intersection of the perpendicular
+        float diffToStart = directionX * (from.x-begin.x)
+        +directionY * (from.y-begin.y);
+        // dividing by length gives absolute offset, dividing twice gives relative
+        return (float)diffToStart / (float)lengthSqr;
+        
+    }
+    
+    /** Returns the closest point on an edge with several points.
+     *
+     * It's final so that no one can damage the implementation (and for speed).
+     *
+     */
+    public static final Point getClosestPoint(Point from, FigEdgePoly to) {
+        
+        // get a new Vector with the points - very memory consuming :-(
+        // to improve this, the class FigPoly must provide better accessors
+        Vector points;
+        try{
+            points = ((FigPoly)to._fig).getPointsVector();
+        }catch(Exception e) { // NullPtr or ClassCast
+            points = new Vector();
         }
-
-        public static AnnotationHelper instance(){
-        	if (theInstance==null) theInstance = new AnnotationHelper();
-        	return theInstance;
+        
+        // workaround when the fig has no points
+        if (points.size()<2){
+            points.add(to.getSourcePortFig().center());
+            points.add(to.getDestPortFig().center());
         }
-        public int getNormOffset(Point p0, Point p1, Point p2){
-		// p0: annotation's position
-	  	// p1: edge's starting point
-  		// p2: edge's ending point
-		dd = (float)( ( (p2.x-p1.x)*(p0.y-p1.y) - (p0.x-p1.x)*(p2.y-p1.y) ) /
-  			    Math.sqrt( (p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y) )  );
-  		offset = -1 * (int) Math.round(dd);
-	  	//System.out.println("distance: " + d);
-  		return offset;
-  	}
-
-	public float getRatio(Point r1, Point r0, Point r2){
-    		// r1: position of annotation;
-	  	// r0: edge's starting point
-  		// r2: edge's ending point
-  	 	//edge's direction vector
-		a.x = r2.x-r0.x; a.y = r2.y-r0.y;
-		float t = (float) ( (r1.x-r0.x)*a.x + (r1.y-r0.y)*a.y ) / (a.x*a.x + a.y*a.y);
-	        //calculate normpoint
-		normpoint.x = (int) (  r1.x + (r0.x - r1.x + t*a.x)    );
-		normpoint.y = (int) (  r1.y + (r0.y - r1.y + t*a.y)    );
-	   	//calculate ratio
-		float ges = (float) (Math.sqrt( (r2.x-r0.x)*(r2.x-r0.x) + (r2.y-r0.y)*(r2.y-r0.y) ));
-		float srcToL = (float) (Math.sqrt( (normpoint.x-r0.x)*(normpoint.x-r0.x) + (normpoint.y-r0.y)*(normpoint.y-r0.y) ) );
-		float rest = ges-srcToL;
-		float proz = (srcToL/ges) *100;
-	  	return srcToL/ges;
-	}
-
-        // get the closest point to p on an edge from start to end
-        public Point getClosestPointOnEdge(Point p, Point start, Point end){
-                normpoint = getNormPointOnEdge(p, start, end);
-                // check if the normpoint is ON the edge (not only on the edge's beam)
-                if (  ( (normpoint.x >= start.x && normpoint.x <= end.x) || (normpoint.x <= start.x && normpoint.x >= end.x) )
-                	&& ( (normpoint.y >= start.y && normpoint.y <= end.y) || (normpoint.y <= start.y && normpoint.y >= end.y) )  ){
-                	return normpoint;
-                }
-                else{
-                	if (sqr_distance(normpoint, start) <= sqr_distance(normpoint, end)) return start;
-                	else return end;
-                }
-        }
-
-	// sqr distance of two points
-        public int sqr_distance(Point p1, Point p2){
-                return (int) ( (p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y) );
-        }
-
-	public Point getNormPointOnEdge(Point r1, Point r0, Point r2){
-    		// r1: position of annotation;
-	  	// r0: edge's starting point
-  		// r2: edge's ending point
-  	 	//edge's direction vector
-		a.x = r2.x-r0.x; a.y = r2.y-r0.y;
-		//
-		float t = (float) ( (r1.x-r0.x)*a.x + (r1.y-r0.y)*a.y ) / (a.x*a.x + a.y*a.y);
-	        //calculate NormPoint
-		normpoint.x = (int) (  r1.x + (r0.x - r1.x + t*a.x)    );
-		normpoint.y = (int) (  r1.y + (r0.y - r1.y + t*a.y)    );
-		return normpoint;
-	}
-
+        
+        // the point for the first part
+        Point bestPoint = getClosestPointOnEdge(from, (Point)points.elementAt(0),
+						(Point)points.elementAt(1));
+        // we do not want to do this every time
+        int bestDistance = sqr_distance(from, bestPoint);
+        
+        // second to last part
+        for (int i=1; i<points.size()-1; i++) {
+            Point one = (Point)points.elementAt(i);
+            Point two = (Point)points.elementAt(i+1);
+            Point candidate = getClosestPointOnEdge(from, one, two);
+            // better: use the new point and distance
+            if (sqr_distance(from, candidate) < bestDistance) {
+                bestPoint = candidate;
+                bestDistance = sqr_distance(from, bestPoint);
+            } // end final better
+        } // end for
+        return bestPoint;
+    } // end getClosestPoint
+    
+    
+    /** Standard geometry here.
+     *
+     * It's final so that no one can damage the implementation (and for speed).
+     *
+     * @param p The point that is somewhere near the edge.
+     * @param begin The begin point of the edge.
+     * @param end The end point of the edge.
+     */
+    public static final Point getClosestPointOnEdge(Point p, Point begin, Point end) {
+        // direction of the segment
+        double directionX = (double)(end.x-begin.x);
+        double directionY = (double)(end.y-begin.y);
+        // length of the segment
+        double length = Math.sqrt(directionX*directionX+directionY*directionY);
+        // normalize direction
+        directionX /= length;
+        directionY /= length;
+        
+        // begin + diffToStart*direction is the intersection of the perpendicular
+        double diffToStart = directionX * (p.x-begin.x)
+        +directionY * (p.y-begin.y);
+        
+        if (diffToStart<0.0) // use start or end point if too far out
+            return begin;
+        else if (diffToStart>length)
+            return end;
+        else
+            // if on line, use it;
+            return new Point(begin.x + (int)(diffToStart*directionX),
+            begin.y + (int)(diffToStart*directionY));
+        
+    } // end getCPOE
+    
+    /** sqr distance of two points; unmodified method */
+    public static final int sqr_distance(Point p1, Point p2){
+        return (int) ( (p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y) );
+    }
+    
+    public static Point getNormPointOnEdge(Point r1, Point r0, Point r2){
+        // r1: position of annotation;
+        // r0: edge's starting point
+        // r2: edge's ending point
+        //edge's direction vector
+        Point a = new Point(r2.x-r0.x, r2.y-r0.y);
+        //
+        float t = (float) ( (r1.x-r0.x)*a.x + (r1.y-r0.y)*a.y ) / (a.x*a.x + a.y*a.y);
+        //calculate NormPoint
+        Point normpoint = new Point();
+        normpoint.x = (int) (  r1.x + (r0.x - r1.x + t*a.x)    );
+        normpoint.y = (int) (  r1.y + (r0.y - r1.y + t*a.y)    );
+        return normpoint;
+    }
+    
+    
 }// end of class
--- src/org/tigris/gef/presentation/EdgeAnnotationStrategy.java
+++ src/org/tigris/gef/presentation/EdgeAnnotationStrategy.java
@@ -5,159 +5,151 @@ import java.awt.*;
 import org.tigris.gef.base.*;
 
 
-
 public class EdgeAnnotationStrategy extends AnnotationStrategy{
 
-	AnnotationHelper helper = AnnotationHelper.instance();
-
-	public EdgeAnnotationStrategy(){
-	}
-
-	/*
-	protected boolean lineIsVisible(Fig annotation){ 
-		System.out.println("-----------------------------------------LineIsVisible called !");
-		if (annotation instanceof FigText){
-			return (!((FigText)annotation).getText().equals(""));
+    AnnotationHelper helper = AnnotationHelper.instance();
+    
+    public EdgeAnnotationStrategy(){
+    }
+    
+    public Point restoreAnnotationPosition(Fig annotation){
+	int d; //offset
+	float ratio;
+	
+	Fig owner = annotation.getAnnotationOwner();
+	// in the case: owner is an edge
+	if (owner instanceof FigEdge){
+	    AnnotationProperties prop = (AnnotationProperties)annotations.get(annotation);
+	    d 	= prop.getOffset();
+	    ratio 	= prop.getRatio();
+	    FigEdge edge = (FigEdge)owner;
+	    //Point start 	= edge.getSourcePortFig().center();
+	    //Point ende  	= edge.getDestPortFig().center();
+	    Point start;
+	    Point ende;
+	    try{
+		start = edge.getFirstPoint();
+		ende = edge.getLastPoint();
+	    } catch (ArrayIndexOutOfBoundsException e){
+		try{
+		    start= edge.getSourcePortFig().center();
+		    ende = edge.getDestPortFig().center();
 		}
-		return true;
-	};
-	*/
-
-        public Point restoreAnnotationPosition(Fig annotation){
-		int d; //offset
-		float ratio;
-		
-		Fig owner = annotation.getAnnotationOwner();
-		// in the case: owner is an edge
-		if (owner instanceof FigEdge){
-			AnnotationProperties prop = (AnnotationProperties)annotations.get(annotation);
-			d 	= prop.getOffset();
-			ratio 	= prop.getRatio();
-			FigEdge edge = (FigEdge)owner;
-			//Point start 	= edge.getSourcePortFig().center();
-			//Point ende  	= edge.getDestPortFig().center();
-			Point start;
-			Point ende;
-			try{
-				start = edge.getFirstPoint();
-				ende = edge.getLastPoint();
-			} catch (ArrayIndexOutOfBoundsException e){
-				try{
-					start= edge.getSourcePortFig().center();
-					ende = edge.getDestPortFig().center();
-				}
-				catch(NullPointerException ne){
-    					start = new Point(10,10);
-    					ende = new Point(100,10);
-				}
-			}
-
-			if ((start.x == ende.x) && (start.y == ende.y)) return annotation.getLocation();
-			
-			// calculate
-			float xdirection = ende.x - start.x;
-			float ydirection = ende.y - start.y;
-			double newX = start.x + ratio * xdirection;
-			double newY = start.y + ratio * ydirection;
-			// restore offset d
-			newX = newX + d * ( ydirection / Math.sqrt(xdirection*xdirection + ydirection*ydirection) );
-			newY = newY + d * (-1)* (xdirection / Math.sqrt(xdirection*xdirection + ydirection*ydirection) );
-			// the annotation's new position
-			return new Point( (int)newX- (annotation.getWidth()/2), (int)newY-(annotation.getHeight()/2) );
+		catch(NullPointerException ne){
+		    start = new Point(10,10);
+		    ende = new Point(100,10);
 		}
-		return new Point(1,1);
+	    }
+
+	    if ((start.x == ende.x) && (start.y == ende.y)) return annotation.getLocation();
+
+	    // calculate
+	    float xdirection = ende.x - start.x;
+	    float ydirection = ende.y - start.y;
+	    double newX = start.x + ratio * xdirection;
+	    double newY = start.y + ratio * ydirection;
+	    // restore offset d
+	    newX = newX + d * ( ydirection / Math.sqrt(xdirection*xdirection + ydirection*ydirection) );
+	    newY = newY + d * (-1)* (xdirection / Math.sqrt(xdirection*xdirection + ydirection*ydirection) );
+	    // the annotation's new position
+	    return new Point( (int)newX- (annotation.getWidth()/2), (int)newY-(annotation.getHeight()/2) );
 	}
+	return new Point(1,1);
+    }
 
 
-	// calculate offset and ratio of annotation (relative to annotationOwner)
-	// store values in a hashtable
-	// method is called when the annotation is moved without its owner
-	public void storeAnnotationPosition(Fig annotation){
-		int d; float ratio;
-		Fig owner = annotation.getAnnotationOwner();
-		// case: owner ist eine Kante
-		if (owner instanceof FigEdge){
-			FigEdge edge = (FigEdge)owner;
-			Point anPos 	= annotation.center();
-			//Point start= edge.getSourcePortFig().center();	
-			//Point ende = edge.getDestPortFig().center();
-			Point start; 	
-			Point ende;
-			try{
-				start = edge.getFirstPoint();
-				ende = edge.getLastPoint();
-			} catch (ArrayIndexOutOfBoundsException e){
-				start= edge.getSourcePortFig().center();	
-				ende = edge.getDestPortFig().center();
-			}
-			
-			if ((start.x == ende.x) && (start.y == ende.y)) return;
-			
-			d     = helper.getNormOffset(anPos, start, ende);
-			ratio = helper.getRatio(anPos, start, ende);
-                        Point normpoint = helper.getNormPointOnEdge(anPos, start, ende);
-			// store values
-			AnnotationProperties prop = (AnnotationProperties)annotations.get(annotation);
-			prop.setRatio(ratio,prop.hasFixedRatio());
-			prop.setOffset(d ,prop.hasFixedOffset());
+    /** Calculates offset and ratio of annotation (relative to annotationOwner).
+     * method is called when the annotation is moved without its owner
+     */
+    public void storeAnnotationPosition(Fig annotation){
+	Fig owner = annotation.getAnnotationOwner();
+	// case: owner ist eine Kante
+	if (owner instanceof FigEdge){
+	    FigEdge edge = (FigEdge)owner;
+	    Point anPos 	= annotation.center();
+	    //Point start= edge.getSourcePortFig().center();	
+	    //Point ende = edge.getDestPortFig().center();
+	    Point start; 	
+	    Point ende;
+	    try{
+		start = edge.getFirstPoint();
+		ende = edge.getLastPoint();
+	    } catch (ArrayIndexOutOfBoundsException e){
+		start= edge.getSourcePortFig().center();	
+		ende = edge.getDestPortFig().center();
+	    }
 
-			//((PathConvPercentPlusConst)edge.getPathConvOfFig(annotation)).setPercentOffset((int)ratio, d);
-			//System.out.println("ratio = " +ratio + "   d= " +d);
+	    if ((start.x == ende.x) && (start.y == ende.y)) return;
 
-			//
-		}
-		drawConnectingLine(annotation);
+	    int d = AnnotationHelper.getNormOffset(anPos, start, ende);
+	    float ratio = AnnotationHelper.getRatio(anPos, start, ende);
+	    // store values
+	    AnnotationProperties prop = getAnnotationProperties(annotation);
+	    prop.setRatio(ratio,prop.hasFixedRatio());
+	    prop.setOffset(d ,prop.hasFixedOffset());
 	}
+	drawConnectingLine(annotation);
+    }
 
-	// draws a dotted line between this annotation and its owner
-	public void drawConnectingLine(Fig annotation) throws NullPointerException{
-		if (!(getAnnotationProperties(annotation).lineIsVisible(annotation))) return;
-		Fig owner = annotation.getAnnotationOwner();
-		AnnotationProperties prop = (AnnotationProperties)annotations.get(annotation);
-		FigLine line = prop.getConnectingLine();
+    /** Draws a dotted line between this annotation and its owner.
+     */
+    public void drawConnectingLine(Fig annotation) throws NullPointerException{
+	if (!(getAnnotationProperties(annotation).lineIsVisible(annotation))) return;
+	Fig owner = annotation.getAnnotationOwner();
+	AnnotationProperties prop = getAnnotationProperties(annotation);
+	FigLine line = prop.getConnectingLine();
 
-		if (  ((FigEdge)owner).getSourcePortFig().center() == null || ((FigEdge)owner).getDestPortFig().center() == null ) return;
-                //line from annotation to center of owning edge
-		//line.setShape(annotation.center(), owner.center());
-                //line from annotation to closest point on owning edge
-                try{
-                	line.setShape(annotation.center(), helper.getClosestPointOnEdge(annotation.center(),((FigEdge)owner).getFirstPoint(), ((FigEdge)owner).getLastPoint()));
-                } catch (ArrayIndexOutOfBoundsException e){
-                	//line.setShape(annotation.center(), calculateLotpunkt(annotation.center(), ((FigEdge)owner).getSourcePortFig().center(), ((FigEdge)owner).getDestPortFig().center() ));
-                	line.setShape(annotation.center(), helper.getClosestPointOnEdge(annotation.center(), ((FigEdge)owner).getSourcePortFig().center(), ((FigEdge)owner).getDestPortFig().center() ));
-                }
-                //line.setShape(annotation.center(), getClosestPointOnEdge(annotation.center(), ((FigEdge)owner).getSourcePortFig().center(), ((FigEdge)owner).getDestPortFig().center() ));
-                line.setLineColor(getAnnotationProperties(annotation).getLineColor());
-		line.setFillColor(getAnnotationProperties(annotation).getLineColor());
-		line.setDashed(true);
-		// draw the line
-		if (!(Globals.curEditor().getLayerManager().getContents().contains(line))) Globals.curEditor().add(line);
-		Globals.curEditor().getLayerManager().bringToFront(annotation);
-		//
-		line.damage();
-		annotation.damage();
-
-		// remove line automatically
-		AnnotationLineRemover.instance().removeLineIn( getAnnotationProperties(annotation).getLineVisibilityDuration(),annotation );
-        }
-
-	// move annotations
-	// this method is called, when an annotationOwner is moved without its
-	// annotations
-	public void translateAnnotations(Fig owner){
-		java.util.Enumeration enum = annotations.keys();
-		// owner has moved; set annotations to their new positions
-		while (enum.hasMoreElements()){
-			Fig annotation = (Fig)enum.nextElement();
-			annotation.setLocation(restoreAnnotationPosition(annotation));
-			//drawConnectingLine(annotation);
-			Globals.curEditor().getLayerManager().bringToFront(annotation);
-			// call endtrans of annotation
-			annotation.endTrans();
-			annotation.damage();
-
-		}
-		owner.damage();
+	if (  ((FigEdge)owner).getSourcePortFig().center() == null || ((FigEdge)owner).getDestPortFig().center() == null ) 
+	    return;
+	//line from annotation to closest point on owning edge
+	try{
+	    if (owner instanceof FigEdgePoly){
+		line.setShape(annotation.center(), 
+			      AnnotationHelper.getClosestPoint(annotation.center(), (FigEdgePoly)owner));
+	    }else{
+		line.setShape(annotation.center(), 
+			      AnnotationHelper.getClosestPointOnEdge(annotation.center(), 
+								     ((FigEdge)owner).getFirstPoint(), 
+								     ((FigEdge)owner).getLastPoint()));
+	    }
+	} catch (ArrayIndexOutOfBoundsException e){
+	    line.setShape(annotation.center(), 
+			  helper.getClosestPointOnEdge(annotation.center(), 
+						       ((FigEdge)owner).getSourcePortFig().center(), 
+						       ((FigEdge)owner).getDestPortFig().center() ));
 	}
+	line.setLineColor(getAnnotationProperties(annotation).getLineColor());
+	line.setFillColor(getAnnotationProperties(annotation).getLineColor());
+	line.setDashed(true);
+	// draw the line
+	if (!(Globals.curEditor().getLayerManager().getContents().contains(line))) Globals.curEditor().add(line);
+	Globals.curEditor().getLayerManager().bringToFront(annotation);
+	//
+	line.damage();
+	annotation.damage();
+
+	// remove line automatically
+	AnnotationLineRemover.instance().removeLineIn( getAnnotationProperties(annotation).getLineVisibilityDuration(),annotation );
+    }
+
+    /** move annotations.
+     * this method is called when an annotationOwner is moved without its
+     * annotations
+     */
+    public void translateAnnotations(Fig owner){
+	java.util.Enumeration enum = annotations.keys();
+	// owner has moved; set annotations to their new positions
+	while (enum.hasMoreElements()){
+	    Fig annotation = (Fig)enum.nextElement();
+	    annotation.setLocation(restoreAnnotationPosition(annotation));
+	    //drawConnectingLine(annotation);
+	    Globals.curEditor().getLayerManager().bringToFront(annotation);
+	    // call endtrans of annotation
+	    annotation.endTrans();
+	    annotation.damage();
+
+	}
+	owner.damage();
+    }
 
 } // end of class
