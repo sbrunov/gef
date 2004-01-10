78463707d4465a33fe5c8c4c8fe1a2e52f9341b4
Bob Tarling <BobTarling>
2004-01-10 01:52:49 +0000

    Reformat

--- src/org/tigris/gef/base/ModeCreateEdge.java
+++ src/org/tigris/gef/base/ModeCreateEdge.java
@@ -21,8 +21,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-
-
 // File: ModeCreateEdge.java
 // Classes: ModeCreateEdge
 // Original Author: ics125 spring 1996
@@ -30,9 +28,7 @@
 
 package org.tigris.gef.base;
 
-import java.awt.*;
 import java.awt.event.*;
-
 import org.tigris.gef.graph.*;
 import org.tigris.gef.presentation.*;
 
@@ -48,134 +44,163 @@ import org.tigris.gef.presentation.*;
  *  and connecting it to other model elements. */
 
 public class ModeCreateEdge extends ModeCreate {
-  ////////////////////////////////////////////////////////////////
-  // instance variables
+    ////////////////////////////////////////////////////////////////
+    // instance variables
 
-  /** The NetPort where the arc is paintn from */
-  private Object _startPort;
+    /** The NetPort where the arc is paintn from */
+    private Object _startPort;
 
-  /** The Fig that presents the starting NetPort */
-  private Fig _startPortFig;
+    /** The Fig that presents the starting NetPort */
+    private Fig _startPortFig;
 
-  /** The FigNode on the NetNode that owns the start port */
-  private FigNode _sourceFigNode;
+    /** The FigNode on the NetNode that owns the start port */
+    private FigNode _sourceFigNode;
 
-  /** The new NetEdge that is being created */
-  private Object _newEdge;
+    /** The new NetEdge that is being created */
+    private Object _newEdge;
 
-  ////////////////////////////////////////////////////////////////
-  // constructor
+    ////////////////////////////////////////////////////////////////
+    // constructor
 
-  public ModeCreateEdge() { super(); }
-  public ModeCreateEdge(Editor par) { super(par); }
-
-  ////////////////////////////////////////////////////////////////
-  // Mode API
-
-  public String instructions() {
-    return "Drag to define an edge to another port";
-  }
-
-  ////////////////////////////////////////////////////////////////
-  // ModeCreate API
-
-  /** Create the new item that will be drawn. In this case I would
-   *  rather create the FigEdge when I am done. Here I just
-   *  create a rubberband FigLine to show during dragging. */
-  public Fig createNewItem(MouseEvent me, int snapX, int snapY) {
-    return new FigLine(snapX, snapY, 0, 0,
-		       Globals.getPrefs().getRubberbandColor());
-  }
-
-  ////////////////////////////////////////////////////////////////
-  // event handlers
-
-  /** On mousePressed determine what port the user is dragging from.
-   *  The mousePressed event is sent via ModeSelect. */
-  public void mousePressed(MouseEvent me) {
-    if (me.isConsumed()) return;
-    int x = me.getX(), y = me.getY();
-    Editor ce = Globals.curEditor();
-    Fig underMouse = ce.hit(x, y);
-    if (underMouse == null) {
-      //System.out.println("bighit");
-        underMouse = ce.hit(x-16, y-16, 32, 32);
+    public ModeCreateEdge() {
+        super();
     }
-    if (underMouse == null) { done(); me.consume(); return; }
-    if (!(underMouse instanceof FigNode)) { done(); me.consume(); return; }
-    _sourceFigNode = (FigNode) underMouse;
-    _startPort = _sourceFigNode.deepHitPort(x, y);
-    if (_startPort == null) { done(); me.consume(); return; }
-    _startPortFig = _sourceFigNode.getPortFig(_startPort);
-    super.mousePressed(me);
-  }
-
-  /** On mouseReleased, find the destination port, ask the GraphModel
-   *  to connect the two ports.  If that connection is allowed, then
-   *  construct a new FigEdge and add it to the Layer and send it to
-   *  the back. */
-  public void mouseReleased(MouseEvent me) {
-    if (me.isConsumed()) return;
-    if (_sourceFigNode == null) { done(); me.consume(); return; }
-
-    int x = me.getX(), y = me.getY();
-    Class arcClass;
-    Editor ce = Globals.curEditor();
-    Fig f = ce.hit(x, y);
-    if (f == null) { f = ce.hit(x-16, y-16, 32, 32); }
-    GraphModel gm = ce.getGraphModel();
-    if (!(gm instanceof MutableGraphModel)) f = null;
-    MutableGraphModel mgm = (MutableGraphModel) gm;
-    // needs-more-work: potential class cast exception
-
-    if (f instanceof FigNode) {
-      FigNode destFigNode = (FigNode) f;
-      // If its a FigNode, then check within the  
-      // FigNode to see if a port exists 
-      Object foundPort = destFigNode.deepHitPort(x, y);
-
-      //if (foundPort != null && foundPort != _startPort && mgm.canConnect( _startPort, foundPort)) {
-      // NOTE: canConnect should be responsible to decide such things (foundPort != _startPort)
-      if (foundPort != null && mgm.canConnect( _startPort, foundPort)) {
-	Fig destPortFig = destFigNode.getPortFig(foundPort);
-	Class edgeClass = (Class) getArg("edgeClass");
-	if (edgeClass != null)
-	  _newEdge = mgm.connect(_startPort, foundPort, edgeClass);
-	else
-	  _newEdge = mgm.connect(_startPort, foundPort);
-
-	// Calling connect() will add the edge to the GraphModel and
-	// any LayerPersectives on that GraphModel will get a
-	// edgeAdded event and will add an appropriate FigEdge
-	// (determined by the GraphEdgeRenderer).
-
-	if (null != _newEdge) {
-	  LayerManager lm = ce.getLayerManager();
-	  ce.damageAll();
-	  _sourceFigNode.damage();
-	  destFigNode.damage();
-	  _newItem = null;
-
-	  Layer layer= ce.getLayerManager().getActiveLayer();
-
-	  FigEdge fe = (FigEdge)layer.presentationFor(_newEdge);
-	  fe.setSourcePortFig(_startPortFig);
-	  fe.setSourceFigNode(_sourceFigNode);
-	  fe.setDestPortFig(destPortFig);
-	  fe.setDestFigNode(destFigNode);
-	  if (fe != null) ce.getSelectionManager().select(fe);
-	  done();
-	  me.consume();
-	  return;
-	}
-	else
-	  System.out.println("connection return null");
-      }
+    public ModeCreateEdge(Editor par) {
+        super(par);
     }
-    _sourceFigNode.damage();
-    ce.damageAll();
-    _newItem = null;
-    done();
-    me.consume();
-  }
+
+    ////////////////////////////////////////////////////////////////
+    // Mode API
+
+    public String instructions() {
+        return "Drag to define an edge to another port";
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // ModeCreate API
+
+    /** Create the new item that will be drawn. In this case I would
+     *  rather create the FigEdge when I am done. Here I just
+     *  create a rubberband FigLine to show during dragging. */
+    public Fig createNewItem(MouseEvent me, int snapX, int snapY) {
+        return new FigLine(
+            snapX,
+            snapY,
+            0,
+            0,
+            Globals.getPrefs().getRubberbandColor());
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // event handlers
+
+    /** On mousePressed determine what port the user is dragging from.
+     *  The mousePressed event is sent via ModeSelect. */
+    public void mousePressed(MouseEvent me) {
+        if (me.isConsumed())
+            return;
+        int x = me.getX(), y = me.getY();
+        Editor ce = Globals.curEditor();
+        Fig underMouse = ce.hit(x, y);
+        if (underMouse == null) {
+            //System.out.println("bighit");
+            underMouse = ce.hit(x - 16, y - 16, 32, 32);
+        }
+        if (underMouse == null) {
+            done();
+            me.consume();
+            return;
+        }
+        if (!(underMouse instanceof FigNode)) {
+            done();
+            me.consume();
+            return;
+        }
+        _sourceFigNode = (FigNode) underMouse;
+        _startPort = _sourceFigNode.deepHitPort(x, y);
+        if (_startPort == null) {
+            done();
+            me.consume();
+            return;
+        }
+        _startPortFig = _sourceFigNode.getPortFig(_startPort);
+        super.mousePressed(me);
+    }
+
+    /** On mouseReleased, find the destination port, ask the GraphModel
+     *  to connect the two ports.  If that connection is allowed, then
+     *  construct a new FigEdge and add it to the Layer and send it to
+     *  the back. */
+    public void mouseReleased(MouseEvent me) {
+        if (me.isConsumed()) {
+            return;
+        }
+        if (_sourceFigNode == null) {
+            done();
+            me.consume();
+            return;
+        }
+
+        int x = me.getX(), y = me.getY();
+        Editor ce = Globals.curEditor();
+        Fig f = ce.hit(x, y);
+        if (f == null) {
+            f = ce.hit(x - 16, y - 16, 32, 32);
+        }
+        GraphModel gm = ce.getGraphModel();
+        if (!(gm instanceof MutableGraphModel)) {
+            f = null;
+        }
+        MutableGraphModel mgm = (MutableGraphModel) gm;
+        // needs-more-work: potential class cast exception
+
+        if (f instanceof FigNode) {
+            FigNode destFigNode = (FigNode) f;
+            // If its a FigNode, then check within the  
+            // FigNode to see if a port exists 
+            Object foundPort = destFigNode.deepHitPort(x, y);
+
+            //if (foundPort != null && foundPort != _startPort && mgm.canConnect( _startPort, foundPort)) {
+            // NOTE: canConnect should be responsible to decide such things (foundPort != _startPort)
+            if (foundPort != null && mgm.canConnect(_startPort, foundPort)) {
+                Fig destPortFig = destFigNode.getPortFig(foundPort);
+                Class edgeClass = (Class) getArg("edgeClass");
+                if (edgeClass != null)
+                    _newEdge = mgm.connect(_startPort, foundPort, edgeClass);
+                else
+                    _newEdge = mgm.connect(_startPort, foundPort);
+
+                // Calling connect() will add the edge to the GraphModel and
+                // any LayerPersectives on that GraphModel will get a
+                // edgeAdded event and will add an appropriate FigEdge
+                // (determined by the GraphEdgeRenderer).
+
+                if (null != _newEdge) {
+                    ce.damageAll();
+                    _sourceFigNode.damage();
+                    destFigNode.damage();
+                    _newItem = null;
+
+                    Layer layer = ce.getLayerManager().getActiveLayer();
+
+                    FigEdge fe = (FigEdge) layer.presentationFor(_newEdge);
+                    fe.setSourcePortFig(_startPortFig);
+                    fe.setSourceFigNode(_sourceFigNode);
+                    fe.setDestPortFig(destPortFig);
+                    fe.setDestFigNode(destFigNode);
+                    if (fe != null)
+                        ce.getSelectionManager().select(fe);
+                    done();
+                    me.consume();
+                    return;
+                } else
+                    System.out.println("connection return null");
+            }
+        }
+        _sourceFigNode.damage();
+        ce.damageAll();
+        _newItem = null;
+        done();
+        me.consume();
+    }
 } /* end class ModeCreateEdge */
