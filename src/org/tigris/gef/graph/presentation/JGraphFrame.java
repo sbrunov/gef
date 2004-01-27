12568f011d0b52a9504d993b24b28feed6b6efee
Bob Tarling <BobTarling>
2004-01-27 20:50:38 +0000

    Remove unused imports

--- src/org/tigris/gef/graph/presentation/JGraphFrame.java
+++ src/org/tigris/gef/graph/presentation/JGraphFrame.java
@@ -24,11 +24,8 @@
 package org.tigris.gef.graph.presentation;
 
 import java.awt.*;
-import java.util.*;
-import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
 import javax.swing.border.*;
 
 import org.tigris.gef.base.*;
