8a936e0c428127925a1111d68d873aab8b010066
Bob Tarling <BobTarling>
2004-04-16 16:45:35 +0000

    Commited patch 114 for fixing ugly arrow heads

--- src/org/tigris/gef/presentation/ArrowHead.java
+++ src/org/tigris/gef/presentation/ArrowHead.java
@@ -2,14 +2,14 @@
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
+// and this paragraph appear in all copies. This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -31,7 +31,7 @@ package org.tigris.gef.presentation;
 import java.awt.*;
 
 /**
- *  Abstract class to draw arrow heads on the ends of FigEdges.
+ * Abstract class to draw arrow heads on the ends of FigEdges.
  */
 
 public abstract class ArrowHead implements java.io.Serializable {
@@ -40,61 +40,53 @@ public abstract class ArrowHead implements java.io.Serializable {
     protected Color arrowLineColor = Color.black;
     protected Color arrowFillColor = Color.black;
 
-
-    public ArrowHead() { }
-
+    public ArrowHead() {
+    }
 
     public ArrowHead(Color line, Color fill) {
         setLineColor(line);
         setFillColor(fill);
     }
 
-
     public Color getLineColor() {
         return arrowLineColor;
     }
-
 
     public void setLineColor(Color newColor) {
         arrowLineColor = newColor;
     }
 
-
     public Color getFillColor() {
         return arrowFillColor;
     }
-
 
     public void setFillColor(Color newColor) {
         arrowFillColor = newColor;
     }
 
-
     public abstract void paint(Graphics g, Point start, Point end);
-
 
     public void paintAtHead(Graphics g, Fig path) {
         Graphics2D g2 = (Graphics2D) g;
         Stroke oldStroke = g2.getStroke();
-        g2.setStroke( new BasicStroke( path.getLineWidth()));
-        paint(g2, path.pointAlongPerimeter(5), path.pointAlongPerimeter(0));
-        g2.setStroke( oldStroke );
+        g2.setStroke(new BasicStroke(path.getLineWidth()));
+        paint(g2, path.getLastPoint(), path.getFirstPoint());
+        g2.setStroke(oldStroke);
     }
 
     public void paintAtTail(Graphics g, Fig path) {
         Graphics2D g2 = (Graphics2D) g;
         Stroke oldStroke = g2.getStroke();
-        g2.setStroke( new BasicStroke( path.getLineWidth() ) );
+        g2.setStroke(new BasicStroke(path.getLineWidth()));
         int len = path.getPerimeterLength();
-        paint(g2, path.pointAlongPerimeter(len-6), path.pointAlongPerimeter(len-1));
-        g2.setStroke( oldStroke );
+        paint(g2, path.getFirstPoint(), path.getLastPoint());
+        g2.setStroke(oldStroke);
     }
-
     /** return the approximate arc length of the path in pixel units */
     public int getLineLength(Point one, Point two) {
         int dxdx = (two.x - one.x) * (two.x - one.x);
         int dydy = (two.y - one.y) * (two.y - one.y);
-        //System.out.println("    ! pall dx, dy = " + dxdx + " , " + dydy);
+        //System.out.println(" ! pall dx, dy = " + dxdx + " , " + dydy);
         return (int) Math.sqrt(dxdx + dydy);
     }
 
@@ -102,25 +94,24 @@ public abstract class ArrowHead implements java.io.Serializable {
     public Point pointAlongLine(Point one, Point two, int dist) {
         int len = getLineLength(one, two);
         int p = dist;
-        if ( len == 0 ) {
+        if (len == 0) {
             return one;
         }
-        //System.out.println("    ! pall dist, len = " + dist + " , " + len);
-        return new Point(one.x + ((two.x - one.x) * p) / len,
-                         one.y + ((two.y - one.y) * p) / len);
+        //System.out.println(" ! pall dist, len = " + dist + " , " + len);
+        return new Point(
+            one.x + ((two.x - one.x) * p) / len,
+            one.y + ((two.y - one.y) * p) / len);
     }
-
 
     public double dist(int x0, int y0, int x1, int y1) {
         double dx;
         double dy;
-        dx = (double)(x0-x1);
-        dy = (double)(y0-y1);
-        return Math.sqrt(dx*dx+dy*dy);
+        dx = (double) (x0 - x1);
+        dy = (double) (y0 - y1);
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
-
     public double dist(double dx, double dy) {
-        return Math.sqrt(dx*dx+dy*dy);
+        return Math.sqrt(dx * dx + dy * dy);
     }
 } /* end class ArrowHead */
