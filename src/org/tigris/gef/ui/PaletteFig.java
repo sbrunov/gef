0671377f5ddaf3e191258de6ce01b412e69796c3
Bob Tarling <BobTarling>
2006-08-26 10:54:47 +0000

    Tidy

--- src/org/tigris/gef/ui/PaletteFig.java
+++ src/org/tigris/gef/ui/PaletteFig.java
@@ -31,11 +31,18 @@
 
 package org.tigris.gef.ui;
 
-import java.awt.*;
-import java.util.*;
-import javax.swing.*;
+import org.tigris.gef.base.CmdSetMode;
+import org.tigris.gef.base.ModeBroom;
+import org.tigris.gef.base.ModeCreateFigCircle;
+import org.tigris.gef.base.ModeCreateFigInk;
+import org.tigris.gef.base.ModeCreateFigLine;
+import org.tigris.gef.base.ModeCreateFigPoly;
+import org.tigris.gef.base.ModeCreateFigRRect;
+import org.tigris.gef.base.ModeCreateFigRect;
+import org.tigris.gef.base.ModeCreateFigSpline;
+import org.tigris.gef.base.ModeCreateFigText;
+import org.tigris.gef.base.ModeSelect;
 
-import org.tigris.gef.base.*;
 
 /** A Palette that defines buttons to create lines, rectangles,
  *  rounded rectangles, circles, and text. Also a select button is
@@ -54,37 +61,36 @@ import org.tigris.gef.base.*;
 
 public class PaletteFig extends ToolBar {
 
-  public PaletteFig() { defineButtons(); }
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 304194274216578087L;
 
+
+    public PaletteFig() {
+        defineButtons();
+    }
   
-  /** Defined the buttons in this palette. Each of these buttons is
-   *  associated with an CmdSetMode, and that Cmd sets the next
-   *  global Mode to somethign appropriate. All the buttons can stick
-   *  except 'select'. If the user unclicks the sticky checkbox, the
-   *  'select' button is automatically pressed. */
-  public void defineButtons() {
-    add(new CmdSetMode(ModeSelect.class, "Select"));
-    add(new CmdSetMode(ModeBroom.class, "Broom"));
-    addSeparator();
-    add(new CmdSetMode(ModeCreateFigCircle.class, "Circle"));
-    add(new CmdSetMode(ModeCreateFigRect.class, "Rectangle"));
-    add(new CmdSetMode(ModeCreateFigRRect.class, "RRect"));
-    add(new CmdSetMode(ModeCreateFigLine.class, "Line"));
-    add(new CmdSetMode(ModeCreateFigText.class, "Text"));
-    add(new CmdSetMode(ModeCreateFigPoly.class, "Polygon"));
-    add(new CmdSetMode(ModeCreateFigSpline.class, "Spline"));
-    add(new CmdSetMode(ModeCreateFigInk.class, "Ink"));
-
-//     circleButton.canStick(true);
-//     rectButton.canStick(true);
-//     rRectButton.canStick(true);
-//     lineButton.canStick(true);
-//     textButton.canStick(true);
-//     polyButton.canStick(true);
-//     inkButton.canStick(true);
-//     image1Button.canStick(true);
-//     image2Button.canStick(true);
-  }
+    /**
+     * Defined the buttons in this palette. Each of these buttons is
+     * associated with an CmdSetMode, and that Cmd sets the next
+     * global Mode to somethign appropriate. All the buttons can stick
+     * except 'select'. If the user unclicks the sticky checkbox, the
+     * 'select' button is automatically pressed.
+     */
+    public void defineButtons() {
+        add(new CmdSetMode(ModeSelect.class, "Select"));
+        add(new CmdSetMode(ModeBroom.class, "Broom"));
+        addSeparator();
+        add(new CmdSetMode(ModeCreateFigCircle.class, "Circle"));
+        add(new CmdSetMode(ModeCreateFigRect.class, "Rectangle"));
+        add(new CmdSetMode(ModeCreateFigRRect.class, "RRect"));
+        add(new CmdSetMode(ModeCreateFigLine.class, "Line"));
+        add(new CmdSetMode(ModeCreateFigText.class, "Text"));
+        add(new CmdSetMode(ModeCreateFigPoly.class, "Polygon"));
+        add(new CmdSetMode(ModeCreateFigSpline.class, "Spline"));
+        add(new CmdSetMode(ModeCreateFigInk.class, "Ink"));
+    }
 } /* end class PaletteFig */
 
 
