0dff91cf90562701cd340d53cb6883ae00d247dc
Bob Tarling <BobTarling>
2007-08-04 23:48:24 +0000

    Provide an empty implementation for SWT

---
+++ trunk/src/org/tigris/gef/swt/SwtPresentation.java
@@ -0,0 +1,16 @@
+package org.tigris.gef.swt;
+
+import org.tigris.gef.base.Layer;
+import org.tigris.gef.graph.presentation.GraphFrame;
+import org.tigris.gef.graph.presentation.Presentation;
+
+public class SwtPresentation implements Presentation {
+
+    public Layer createLayerGrid() {
+	return null;
+    }
+    
+    public GraphFrame createGraphFrame() {
+	return null;
+    }
+}
